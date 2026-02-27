43c392518051ea1b6347e8289fbc8e01a74b90e4
Civi <civi@rcg.fun>
2026-02-27 12:08:31 +0800

    fix

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,10 +1,11 @@
-{ lib, config, ... }:
+{ lib, pkgs, ... }:
 {
   _class = "homeManager";
   imports = [
     (lib.modules.importApply ../default.nix {
-      everforestModules =
-        if config._module.args.stdenv.hostPlatform.isDarwin then import ./darwin-modules.nix else import ./all-modules.nix;
+      everforestModules = (lib.optional (pkgs.stdenv.isLinux) ./all-modules.nix) (
+        lib.optional (pkgs.stdenv.isDarwin) ./darwin-modules.nix
+      );
     })
   ];
 }
